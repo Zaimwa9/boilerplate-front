a6fe363b73427ca8299fd0f6dc76e4540ec3fc95
Wadii <B00549848@essec.edu>
2022-01-27 22:07:23 +0100

    boilerplate: provide-store-to-app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,26 @@
 import { Header, BCol, BRow } from "design-system/index";
 import { Container } from "react-bootstrap";
+import { Provider } from "react-redux";
+import store from "../app/store";
+
+const AppPlaceholder = () => (
+  <Container>
+    <BRow>
+      <BCol>
+        <Header />
+      </BCol>
+    </BRow>
+    <BRow>
+      <BCol justify="center">Lorem Ipsum</BCol>
+    </BRow>
+  </Container>
+);
 
 function App() {
   return (
-    <Container>
-      <BRow>
-        <BCol>
-          <Header />
-        </BCol>
-      </BRow>
-      <BRow>
-        <BCol justify="center">Lorem Ipsum</BCol>
-      </BRow>
-    </Container>
+    <Provider store={store}>
+      <AppPlaceholder />;
+    </Provider>
   );
 }
 
